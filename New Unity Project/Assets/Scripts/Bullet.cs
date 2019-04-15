d42dad5aa4acc3c992d9a59f8cc8ec9050a643b2
Maherj <Maherj.jaber23@gmail.com>
2019-04-15 16:21:18 +0200

    spell combo and stuff

---
+++ New Unity Project/Assets/Scripts/Bullet.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    float moveSpeed = 8f;
+    float deleteTime = 2f;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            Destroy(gameObject);
+        }
+
+
+    }
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        Vector3 Direction = new Vector3(0, moveSpeed * Time.deltaTime);
+        pos += transform.rotation * Direction;
+        transform.position = pos;
+
+        deleteTime -= Time.deltaTime;
+
+        if (deleteTime <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
---
+++ New Unity Project/Assets/Scripts/playerInput.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerInput : MonoBehaviour
+{
+
+    public GameObject[] spells;
+    private float shootingDelay = 0.55f;
+    private float cooldownTimer = 0.6f;
+    public static int currentspell;
+
+
+    void Update()
+    {
+
+
+
+        cooldownTimer -= Time.deltaTime;
+
+        if (Input.GetButton("Fire2") && Input.GetButton("Fire3") && cooldownTimer <= 0)
+        {
+            cooldownTimer = shootingDelay;
+            currentspell = 0;
+
+            Instantiate(spells[currentspell], transform.position, transform.rotation);
+
+
+        }
+        else if (Input.GetButton("Fire2") && Input.GetButton("Fire1") && cooldownTimer <= 0)
+        {
+            cooldownTimer = shootingDelay;
+            currentspell = 1;
+
+            Instantiate(spells[currentspell], transform.position, transform.rotation);
+
+        }
+    }
+
+}
